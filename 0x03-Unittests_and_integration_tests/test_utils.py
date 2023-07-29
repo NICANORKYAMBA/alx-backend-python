dac364cccac058858de331d76a2e7604d8ea465e
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-07-29 14:54:09 +0300

    Fixed code failure

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -81,6 +81,24 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(get_json(test_url), test_payload)
 
 
+class TestClass:
+    """
+    Test class
+    """
+    def a_method(self):
+        """
+        Test method
+        """
+        return 42
+
+    @memoize
+    def a_property(self):
+        """
+        Test property
+        """
+        return self.a_method()
+                
+                
 class TestMemoize(unittest.TestCase):
     """
     Test class for memoize method
@@ -89,30 +107,13 @@ class TestMemoize(unittest.TestCase):
         """
         Test method for memoize
         """
-        class TestClass:
-            """
-            Test class
-            """
-            def a_method(self):
-                """
-                Test method
-                """
-                return 42
-
-            @memoize
-            def a_property(self):
-                """
-                Test property
-                """
-                return self.a_method()
-
         test_instance = TestClass()
 
         with patch.object(test_instance, 'a_method') as mock_a_method:
             mock_a_method.return_value = 42
 
-            result_1 = test_instance.a_property()
-            result_2 = test_instance.a_property()
+            result_1 = test_instance.a_property
+            result_2 = test_instance.a_property
 
             mock_a_method.assert_called_once()
 
